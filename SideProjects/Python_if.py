c0d51cd6e24c3eb656671f35b69b88d6f1461fba
Kaia F <kaialforrester@gmail.com>
2021-04-16 20:18:56 -0700

    Nice or Mean game
    along with little side projects for learning

---
+++ SideProjects/Python_if.py
@@ -0,0 +1,14 @@
+
+
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key: 
+        print("Num1 is equal to twelve and they have the key!")
+    else:
+        print("Num1 is equal to tweleve and they DONT have the key!")
+elif num1 < 12:
+    print("Num1 is less than twelve!")
+else:
+    print("Num1 is NOT equal to twelve!")
---
+++ SideProjects/myQuiz.py
@@ -0,0 +1,7 @@
+import Budget
+
+
+
+
+
+print(Budget.calcBills())
