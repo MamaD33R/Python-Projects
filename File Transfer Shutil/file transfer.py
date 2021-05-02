9022273adef68e19196f1be83f8d32d5c9e32a4a
Kaia F <kaialforrester@gmail.com>
2021-05-02 14:00:54 -0700

    Update to File Transfer w/ Shutil

--- File Transfer Shutil/file transfer.py
+++ File Transfer Shutil/file transfer.py
@@ -4,27 +4,27 @@ from tkinter import *
 from datetime import timedelta
 from tkinter import filedialog
 
-sourc = 'C:/Users/space/OneDrive/Documents/GitHub/Python-Projects/File Transfer Shutil/Files'
-dest = 'C:/Users/space/OneDrive/Documents/GitHub/Python-Projects/File Transfer Shutil/Updated'
-
-files = os.listdir(sourc) 
-files.sort()
-time = os.path.getmtime(sourc)
-datetime = datetime.datetime.fromtimestamp(time)
-
-def filechecking(files):
-    for f in files:
-        print(f)
-        src = sourc+f
-        dst = dest+f
-        try:
-            if datetime.now() - timedelta(hours=24):
-                path = os.path.join(src, f)
-                shutil.move(path,dest)
-        except OSError:
-            pass
-
 root = Tk()
+
+srcEntry = Entry(root)
+srcEntry.grid(column=4, row=3)
+
+destEntry = Entry(root)
+destEntry.grid(column=5, row=3)
+
+def filechecking():
+    #grabs list of files from source path this way
+    files = os.listdir(srcEntry.get())
+    for file in files:
+        fullFilePath = srcEntry.get() + '/' + file
+        # getting mtime of full file path and name
+        time = os.path.getmtime(fullFilePath)
+        modtime = datetime.datetime.fromtimestamp(time)
+        if modtime > (datetime.datetime.now() - timedelta(hours = 24)):
+            shutil.move(fullFilePath, destEntry.get())
+            # any modtime within 24 hours will be moved to dest folder
+
+
 root.title("File Check")
 root.geometry('500x400')
 root.resizable(False, False)
@@ -34,17 +34,23 @@ lbl.grid(row=1, column=4, pady=20,padx=20)
 btn1 = Button(root, text="Browse Unchecked", command=lambda:browse_button())
 btn1.grid(column=4,row=7,padx=5,pady=5)
 
-btn1 = Button(root, text="Browse Receiving Folders", command=lambda:browse_button())
+btn1 = Button(root, text="Browse Receiving Folders", command=lambda:browse_button2())
 btn1.grid(column=4,row=8,padx=5,pady=5)
 
-btn1 = Button(root, text="Check for updated files", command=lambda:filechecking(files))
+btn1 = Button(root, text="Check for updated files", command=lambda:filechecking())
 btn1.grid(column=4,row=9,padx=5,pady=5)
 
+# gets paths of folders with these functions
 def browse_button():
-    global folder_path
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
-    print(filename)
+    source = filedialog.askdirectory()
+    srcEntry.delete(0, END)
+    srcEntry.insert(END, source)
+
+def browse_button2():
+    dest = filedialog.askdirectory()
+    destEntry.delete(0, END)
+    destEntry.insert(END, dest)
+    
 
 
 
